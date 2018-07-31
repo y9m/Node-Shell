edbe667e62cd80d1c2af26fc3e1c07603cdcef11
Ryan McNierney <ryan.mcnierney@gmail.com>
2018-07-31 12:45:50 -0400

    created pwd.js

--- bash.js
+++ bash.js
@@ -1,11 +1,4 @@
 process.stdout.write('prompt > ');
 
-process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim();
-
-  if (cmd === 'pwd') {
-    process.stdout.write(process.cwd());
-  }
-
-  process.stdout.write('\nprompt > ');
-});
+const pwd = require('./pwd.js');
+pwd();
